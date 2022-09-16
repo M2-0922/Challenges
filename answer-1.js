c188d803bc78980411ac7919cc656ae48ea0f7b8
Aya <pyon220@gmail.com>
2022-09-16 14:37:28 -0700

    assignment finished

--- answer-1.js
+++ answer-1.js
@@ -1,6 +1,35 @@
-/*
+// capitalize(‘a short sentence’) → ‘A Short Sentence’
+let sentence = "a short sentence";
+let temp;
+for (let x = 0; x < sentence.length; x++) {
+  if (x === 0) {
+    temp = sentence[0].toUpperCase();
+  } else if (sentence[x - 1] === " ") {
+    temp = temp + sentence[x].toUpperCase();
+  } else temp = temp + sentence[x];
+}
+console.log(temp);
 
-Please put your code under this comment block. You can try your logic
-Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
+//capitalize(‘a lazy fox’) → ‘A Lazy Fox’
+sentence = "a lazy fox";
+temp = "";
+for (let x = 0; x < sentence.length; x++) {
+  if (x === 0) {
+    temp = sentence[0].toUpperCase();
+  } else if (sentence[x - 1] === " ") {
+    temp = temp + sentence[x].toUpperCase();
+  } else temp = temp + sentence[x];
+}
+console.log(temp);
 
-*/
+// capitalize(‘look, it is working!’) → ‘Look, It Is Working!’
+sentence = "look, it is working!";
+temp = "";
+for (let x = 0; x < sentence.length; x++) {
+  if (x === 0) {
+    temp = sentence[0].toUpperCase();
+  } else if (sentence[x - 1] === " ") {
+    temp = temp + sentence[x].toUpperCase();
+  } else temp = temp + sentence[x];
+}
+console.log(temp);
