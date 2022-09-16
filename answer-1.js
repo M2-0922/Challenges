aa6688d0670b511fe7906ce03748482865ca502c
Aya <pyon220@gmail.com>
2022-09-16 14:43:30 -0700

    modified contents of answer-1.js

--- answer-1.js
+++ answer-1.js
@@ -1,5 +1,11 @@
 // capitalize(‘a short sentence’) → ‘A Short Sentence’
 let sentence = "a short sentence";
+//capitalize(‘a lazy fox’) → ‘A Lazy Fox’
+let sentence = "a lazy fox";
+// capitalize(‘look, it is working!’) → ‘Look, It Is Working!’
+let sentence = "look, it is working!";
+
+^^^^^^ same from here ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 let temp;
 for (let x = 0; x < sentence.length; x++) {
   if (x === 0) {
@@ -10,26 +16,4 @@ for (let x = 0; x < sentence.length; x++) {
 }
 console.log(temp);
 
-//capitalize(‘a lazy fox’) → ‘A Lazy Fox’
-sentence = "a lazy fox";
-temp = "";
-for (let x = 0; x < sentence.length; x++) {
-  if (x === 0) {
-    temp = sentence[0].toUpperCase();
-  } else if (sentence[x - 1] === " ") {
-    temp = temp + sentence[x].toUpperCase();
-  } else temp = temp + sentence[x];
-}
-console.log(temp);
 
-// capitalize(‘look, it is working!’) → ‘Look, It Is Working!’
-sentence = "look, it is working!";
-temp = "";
-for (let x = 0; x < sentence.length; x++) {
-  if (x === 0) {
-    temp = sentence[0].toUpperCase();
-  } else if (sentence[x - 1] === " ") {
-    temp = temp + sentence[x].toUpperCase();
-  } else temp = temp + sentence[x];
-}
-console.log(temp);
