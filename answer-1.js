ee6ec16434b0fa9c0e87a8261ef6960d80cc4693
Kubilay Cakmak <kubilayckmk@gmail.com>
2022-09-15 22:12:01 -0700

    challenge-1 created

---
+++ answer-1.js
@@ -0,0 +1,6 @@
+/*
+
+Please put your code under this comment block. You can try your logic
+Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
+
+*/
