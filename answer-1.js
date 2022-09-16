7a0f79f4df6dc9130f5623c39d7553b10573762b
Nami Miyashiro <nami.miyashiro@gmail.com>
2022-09-16 13:35:01 -0700

    finished the assignment

--- answer-1.js
+++ answer-1.js
@@ -4,3 +4,22 @@ Please put your code under this comment block. You can try your logic
 Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
 
 */
+
+let str1 = "a short sentence";
+
+let newStr1 = str1.replace(/\b[a-z]/g, char => char.toUpperCase());
+console.log(newStr1);
+
+////////////
+
+let str2 = "a lazy fox";
+
+let newStr2 = str2.replace(/\b[a-z]/g, char => char.toUpperCase());
+console.log(newStr2);
+
+////////////
+
+let str3 = "look, it is working!";
+
+let newStr3 = str3.replace(/\b[a-z]/g, char => char.toUpperCase());
+console.log(newStr3);
