b2301c9236e1213e156f47e8302c414e43e32142
Soma-dev0808 <ec.fate.gt@gmail.com>
2022-09-16 12:41:52 -0700

    Soma Ikeda Answear

--- answer-1.js
+++ answer-1.js
@@ -4,3 +4,19 @@ Please put your code under this comment block. You can try your logic
 Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
 
 */
+const strings = [
+    'a short sentence',
+    'a lazy fox',
+    'look, it is working!'
+];
+
+const capitalize = (str) => {
+
+    // Split strings by space.
+    const splitedStr = str.split(' ');
+
+    // Map splitedStr and replace first letter for each strings.
+    return splitedStr.map((ss) => ss[0].toUpperCase() + ss.substring(1));
+}
+
+strings.forEach(string => console.log(capitalize(string)))
