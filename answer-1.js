d8977ea5c41c1538b47eafa836bdb3f9f3147d76
Mai Watanabe <suemks_24@yahoo.co.jp>
2022-09-17 12:53:02 -0700

    finished the challenges

--- answer-1.js
+++ answer-1.js
@@ -4,3 +4,12 @@ Please put your code under this comment block. You can try your logic
 Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
 
 */
+
+let arr = ["a", "short", "sentence","\n","a", "lazy", "fox","\n", "look,", "it", "is", "working"];
+  
+for(i in arr){
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);   
+ }
+
+ console.log(arr.join(" "));
+
