90f761d9c2de7d6f08b3c3d12177b94811084137
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-18 11:29:40 -0700

    finished the challenges

--- answer-1.js
+++ answer-1.js
@@ -4,3 +4,19 @@ Please put your code under this comment block. You can try your logic
 Then you can add this file to staged area for commit and push to your own Branch ({your-name-with-dash})
 
 */
+
+let cap = function(strings) {
+  const arr = strings.split(/\s/);
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+  }
+  console.log(arr.join(" "));
+}
+
+// a short sentence
+cap("a short sentence");
+// a lazy fox
+cap("a lazy fox");
+// look, it is working!
+cap("look, it is working!");
