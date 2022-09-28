a4707a702ff0a39c4d4c033874e9cde40cfa13de
Kanako Kawakami <beruchan.kawaii420@gmail.com>
2022-09-28 09:40:22 -0700

    second commit answer-3.js

--- answer-3.js
+++ answer-3.js
@@ -1,10 +1,8 @@
 // Write a JavaScript program to calculate the factorial of a number.
 // Output function should return the result as Number.
-// 数値の階乗を計算する JavaScript プログラムを作成します。
 // console.log(factorial(5)) // 120
 // console.log(factorial(6)) // 720
 
-/* 5! */
 function factorial(n){
     if(n <= 1){
         return n;        
@@ -14,16 +12,5 @@ function factorial(n){
 }
 
 console.log(factorial(5));
-
-
-/* 6! */
-function factorial(n){
-    if(n <= 1){
-        return n;        
-    }else{
-        return n * factorial(n - 1);
-    }
-}
-
 console.log(factorial(6));
 
