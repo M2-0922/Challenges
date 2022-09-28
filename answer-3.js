c833825e1344e3893d644ef5e6eb5afbbbfd5a5d
Aya <pyon220@gmail.com>
2022-09-28 09:40:20 -0700

    challenge finished

---
+++ answer-3.js
@@ -0,0 +1,14 @@
+/*Factorial (recursion) Challenge
+Write a JavaScript program to calculate the factorial of a number.
+Output function should return the result as Number.*/
+
+console.log(factorial(5)); // 120
+console.log(factorial(6)); // 720
+
+function factorial(number) {
+  if (number === 1) {
+    return number;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
---
+++ challenge-3.md
@@ -0,0 +1,14 @@
+Factorial (recursion) Challenge
+Write a JavaScript program to calculate the factorial of a number.
+Output function should return the result as Number.
+
+console.log(factorial(5)) // 120
+console.log(factorial(6)) // 720
+
+function factorial(number) {
+if (power == 1) {
+return number;
+} else {
+return number \* pow(number, power - 1);
+}
+}
