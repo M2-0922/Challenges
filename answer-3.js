afe0ac2ff617b2692f7a4eaf5f2f1b7c392cd9e7
Yuya Minamide <dettu1003@gmail.com>
2022-09-28 10:22:33 -0700

    did assignment

---
+++ answer-3.js
@@ -0,0 +1,11 @@
+function factorial(num) {
+    let answer = 1;
+    if (num == 0 || num == 1) {
+        return answer;
+    } else {
+        for(let i = num; i >= 1; i--) {
+        answer = answer * i;
+        }
+    return answer;
+    }  
+}
