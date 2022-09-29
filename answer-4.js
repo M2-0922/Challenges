470bbcddd5a9d21ab1db3bc6cc447be8fbca7d09
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-29 11:30:29 -0700

    finished

--- answer-4.js
+++ answer-4.js
@@ -1,6 +1,6 @@
-function isPrime(n) {
-  for (i = 2; i < n; i++) {
-    if (n % i === 0) {
+function isPrime(number) {
+  for (i = 2; i < number; i++) {
+    if (number % i === 0) {
       return false;
       break;
     }
